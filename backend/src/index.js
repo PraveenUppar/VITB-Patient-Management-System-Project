9e810875960a96328189a459d5f87d90a31f7363
PraveenUppar <144151782+PraveenUppar@users.noreply.github.com>
2024-09-29 21:42:47 +0530

    connected mongodb

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+
+mongoose.connect(
+  "mongodb+srv://praveenuppar718:ZDATCXHlcHdrYVEr@pms.8zjsr.mongodb.net/?retryWrites=true&w=majority&appName=PMS"
+); //
 
 app.listen(7000, () => {
   console.log("server running on localhost:7000");
